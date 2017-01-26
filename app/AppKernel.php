794f796e3ec71f9cbd11f29b9857bfb918be464a
AB <whatzerrname@yahoo.com>
2017-01-26 10:40:15 +0100

    OB Highcharts Update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new \Ob\HighchartsBundle\ObHighchartsBundle(),
+            new \Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
