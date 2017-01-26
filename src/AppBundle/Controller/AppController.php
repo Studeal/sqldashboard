65908343a1e969d8c9f484d4d713a8b67ab0954f
hide06 <hide06@outlook.com>
2017-01-26 16:51:16 +0100

    charts - button update

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ob\HighchartsBundle\Highcharts\Highchart;
 
 class AppController extends Controller
 {
@@ -13,6 +14,18 @@ class AppController extends Controller
 
     public function addComponentAction()
     {
-        return $this->render('AppBundle:App:component.html.twig');
+        $series = array(
+        array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
+        );
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
+        $ob->title->text('Chart Title');
+        $ob->xAxis->title(array('text'  => "Horizontal axis title"));
+        $ob->yAxis->title(array('text'  => "Vertical axis title"));
+        $ob->series($series);
+
+
+        return $this->render('AppBundle:App:component.html.twig', array('chart' => $ob));
     }
 }
