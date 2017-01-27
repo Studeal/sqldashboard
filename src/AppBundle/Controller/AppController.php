9809872e3147490a04c30459a80bf127329a2127
hide06 <hide06@outlook.com>
2017-01-27 14:33:00 +0100

    css update

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -14,18 +14,51 @@ class AppController extends Controller
 
     public function addComponentAction()
     {
+        
+        $mainTitle = "Component creation";
+
+        //Factice charts 
         $series = array(
         array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
         );
 
         $ob = new Highchart();
         $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
-        $ob->title->text('Chart Title');
+        $ob->title->text('');
         $ob->xAxis->title(array('text'  => "Horizontal axis title"));
         $ob->yAxis->title(array('text'  => "Vertical axis title"));
         $ob->series($series);
 
 
-        return $this->render('AppBundle:App:component.html.twig', array('chart' => $ob));
+        return $this->render('AppBundle:App:component.html.twig', array(
+            'chart' => $ob,
+            'componentName' => "",
+            'mainTitle' => $mainTitle
+            ));
+    }
+
+    public function editComponentAction()
+    {
+        
+        $mainTitle = "Component edition";
+
+        //Factice charts
+        $series = array(
+        array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
+        );
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
+        $ob->title->text('');
+        $ob->xAxis->title(array('text'  => "Horizontal axis title"));
+        $ob->yAxis->title(array('text'  => "Vertical axis title"));
+        $ob->series($series);
+
+
+        return $this->render('AppBundle:App:component.html.twig', array(
+            'chart' => $ob,
+            'componentName' => "Component name1",
+            'mainTitle' => $mainTitle
+            ));
     }
 }
