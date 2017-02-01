54286cddff8b6d4c26ee6b1dae05db7026e89b42
Toufik Mazy <mazytoufik@outlook.com>
2017-02-01 17:38:09 +0100

    Added DataFixtures / ORM / LoadUser.php

---
+++ src/AppBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,12 @@
+<?php
+namespace AppBundle\DataFixtures\ORM;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+class LoadUser implements FixtureInterface{
+    public function load (ObjectManager $manager)
+    {
+
+    }
+    }
