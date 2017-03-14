a791ed79dc1ae896fa556ff80272b645aee459cb
whatzerrname <whatzerrname@yahoo.com>
2017-03-14 11:50:03 +0100

    Fixed formatting

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -1,10 +1,12 @@
 <?php
 namespace AppBundle\Form;
+
 use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 class UserType extends AbstractType
 {
     /**
@@ -13,11 +15,10 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName',      'text')
-            ->add('lastName',       'text')
-            ->add('image',          'file', array('data_class' => null))
-            ->add('Submit',         'submit')
-        ;
+            ->add('firstName', 'text')
+            ->add('lastName', 'text')
+            ->add('image', 'file', array('data_class' => null))
+            ->add('Submit', 'submit');
     }
 
     /**
@@ -28,16 +29,23 @@ class UserType extends AbstractType
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\User'
         ));
-    }    /**
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function getBlockPrefix()
     {
         return 'appbundle_user';
-    }    public function getParent()
+    }
+
+    public function getParent()
     {
         return 'FOS\UserBundle\Form\Type\RegistrationFormType';
-    }    public function getName()
+    }
+
+    public function getName()
     {
         return $this->getBlockPrefix();
-    }}
+    }
+}
