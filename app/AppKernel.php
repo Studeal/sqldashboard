6af49185b21cbcbacaecc9fb76d2c43e8aa12ac1
whatzerrname <whatzerrname@yahoo.com>
2017-03-14 10:13:35 +0100

    Removed DoctrineFixturesBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
             new \Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
